474b33bfbd5b24672dc2175e9f28c472b9c1bb2d
hridoykit <hridoy.tsa@gmail.com>
2023-05-08 01:17:22 +0600

    create news n category data loding api

--- index.js
+++ index.js
@@ -6,12 +6,39 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 
 const categories = require('./data/categories.json');
+const news = require('./data/news.json');
 
 app.get('/', (req, res) =>{
-    res.send('empty api calling');
+    res.send('first trial');
 });
-app.get('/categories', (req, res) =>{
+
+app.get('/news-categories', (req, res) =>{
     res.send(categories);
+});
+
+app.get('/category/:id', (req, res) =>{
+    const id = req.params.id;
+
+    if(id === '08'){//id_08 was empty, now show all news with if condition
+
+        res.send(news);
+
+    }else{
+
+        const selectedCategory = news.filter(c => c.category_id === id);
+        res.send(selectedCategory);
+    }
+    
+});
+
+app.get('/news', (req, res) =>{
+    res.send(news);
+});
+
+app.get('/news/:id', (req, res) =>{
+    const id = req.params.id;
+    const selectedNews = news.find(n => n._id === id);
+    res.send(selectedNews);
 });
 
 app.listen(port);
